3a68df9cb7ee6cf83fa0ccd8213593fc0d2dafb9
Toxicwhisper <morgan.lindzen@gmail.com>
2023-11-27 19:19:19 +0100

    updated db context

--- AnonymousForum/Data/AnonymousForumContext.cs
+++ AnonymousForum/Data/AnonymousForumContext.cs
@@ -8,13 +8,13 @@ namespace AnonymousForum.Data
 {
     public class AnonymousForumContext : DbContext
     {
-        public AnonymousForumContext (DbContextOptions<AnonymousForumContext> options)
+        public AnonymousForumContext(DbContextOptions<AnonymousForumContext> options)
             : base(options)
         {
         }
 
         public DbSet<ForumThread> ForumThread { get; set; } = default!;
 
-        public DbSet<Reply>? Reply { get; set; }
+        public DbSet<Reply> Reply { get; set; } = default!;
     }
 }
