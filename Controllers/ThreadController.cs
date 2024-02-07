f5f9b1940be93444a84685469e116db1d98c2490
Toxicwhisper <morgan.lindzen@gmail.com>
2024-02-07 13:05:28 +0100

    updated controller to display threads and created new form to be ablee to create new threads

--- Controllers/ThreadController.cs
+++ Controllers/ThreadController.cs
@@ -25,5 +25,30 @@ namespace AnonymousForum.Controllers
 
             return View(threads);
         }
+        // GET: Thread/Create
+        public IActionResult Create(int topicId)
+        {
+            // topicID to view
+            ViewBag.TopicId = topicId;
+            return View();
+        }
+        // POST: Thread/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AnonymousForum.Models.Thread thread)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new thread to the database
+                _context.Threads.Add(thread);
+                _context.SaveChanges();
+
+                // Redirect to the topic view after creating the thread
+                return RedirectToAction("Index", "Thread", new { topicId = thread.TopicId });
+            }
+
+            // If the model state is not valid, return the view with validation errors
+            return View(thread);
+        }
     }
 }
