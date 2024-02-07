6dfefe59d58580b6edee93414ec0345c29cfa67c
Toxicwhisper <morgan.lindzen@gmail.com>
2024-02-07 12:30:04 +0100

    migration and create view for thread

---
+++ Migrations/20240207112505_AddThreadModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnonymousForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThreadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
